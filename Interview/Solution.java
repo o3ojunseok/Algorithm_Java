3730021737e5b13678b40ff3ee5fff2c4a63ec41
o3ojunseok <o3ojunseok@gmail.com>
2023-12-05 14:08:46 +0000

    Feat: Solution

---
+++ Interview/Solution.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+class Solution {
+  public int[] twoSum(int[] nums, int target) {
+    for (int i = 0; i < nums.length; i++) {
+      for (int j = i + 1; j < nums.length; j++) {
+        if (nums[i] + nums[j] == target) {
+          return new int[]{i, j};
+        }
+      }
+    }
+    return null;
+  }
+
+  public static void main(String[] args) {
+    int[] nums = {2, 7, 11, 15};
+    int target = 9;
+
+    Solution solution = new Solution();
+    int[] result = solution.twoSum(nums, target);
+
+    if (result != null) {
+      System.out.println("Indices: " + Arrays.toString(result));
+    } else {
+      System.out.println("No solution found.");
+    }
+  }
+}
